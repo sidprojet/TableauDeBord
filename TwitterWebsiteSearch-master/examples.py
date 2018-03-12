8eb974b256eb04333a62d2d0a60677e82512b767
Paul Lafaurie <paul.a.lafaurie@gmail.com>
2018-03-12 12:01:51 +0100

    Update du DF, avec les info pour la BD + wordcloud correctement formé

--- TwitterWebsiteSearch-master/examples.py
+++ TwitterWebsiteSearch-master/examples.py
@@ -6,31 +6,34 @@ import matplotlib.pyplot as plt
 from PIL import Image
 from wordcloud import WordCloud
 import numpy as np
+from spacy.lang.fr.stop_words import STOP_WORDS
 ### IMPORTER LES TWEETS
+
+lang = "fr"
 client = TwitterClient()
-query = SearchQuery('légalisation #cannabis  since:2016-01-01 lang:fr')
-path = './collecte/France/'
+query = SearchQuery('légalisation #cannabis  since:2016-01-01 lang:'+lang)
+path = './collecte/'+lang+'/'
 #USER = 'R'
 #OR #marijuana #France  '
 count = 0
 all_tweet = {}
 for page in client.get_search_iterator(query):
     for tweet in page['tweets']:
-        #if tweet['lang'] == 'en':
-        tweet['entities'] = dict(tweet['entities'])
         all_tweet[count] = tweet
         count+=1
     
-        if count%200 == 0:
-            num_fichier = str(len(os.listdir(path)))
-            with open(path+'_cannabis_fr' + num_fichier + '.json', 'a') as f:
-                json.dump(all_tweet, f, ensure_ascii = False)
-            all_tweet = {}
+num_fichier = str(len(os.listdir(path)))
+with open(path+'_cannabis_'+lang + num_fichier + '.json', 'a') as f:
+    json.dump(all_tweet, f, ensure_ascii = False)
+
 
 ### CREER LE DF pour les stocker et les traiter
-df = pd.DataFrame(columns=["lang", "date", "texte", "hashtags"])
+df = pd.DataFrame(columns=["lang", "date", "texte", "hashtags","urls","nb_rt","nb_(y)","user_mentions"])
 directory = os.fsencode(path)
 
+
+hashtags = ""
+texte = ""
 rows = 0
 for file in os.listdir(directory):
     filename = os.fsdecode(file)
@@ -42,37 +45,38 @@ for file in os.listdir(directory):
                 df.at[rows, "date"] =  v["created_at"]
                 df.at[rows, "texte"] = v["text"]
                 df.at[rows, "hashtags"] =  v["entities"]["hashtags"]
+                df.at[rows, "urls"] =  v["entities"]["urls"]
+                df.at[rows, "nb_rt"] =  v["retweet_count"]
+                df.at[rows, "nb_(y)"] =  v["favorite_count"]
+                df.at[rows, "user_mentions"] = v["entities"]["user_mentions"]
+                for word in v["entities"]["hashtags"]:
+                    hashtags += word.capitalize()
+                    hashtags +=" "
+                texte += v["text"].replace("#","").replace("@","")
+                texte += " "
                 rows+=1
         continue
     else:
         continue
 
 ### CREER un nuage de mots!
-
-
-# Read the whole text.
-text = df.hashtags.to_string(header=False,index=False)
-text = text.replace(",", "")
-text = text.replace("[","")
-text = text.replace("]","")
-text = text.replace("\n"," ")
-text = text.replace("\r"," ")
-text = text.replace("é","e")
-for i in range(5):
-    text = text.replace("  ", " ")
-text = text.lower()
 # read the mask image
+
 weed_mask = np.array(Image.open("weed-icon.png"))
 
-#stopwords = set(STOPWORDS)
-#stopwords.add("said")
-
-wc = WordCloud(background_color="white", max_words=2000, mask=weed_mask)
+wc = WordCloud(collocations=False,background_color="white", stopwords = STOP_WORDS,max_words=2000, mask=weed_mask)
 # generate word cloud
-wc.generate(text)
+wc.generate(hashtags)
 
 # store to file
 wc.to_file("WC_WEED.png")
+
+
+# generate word cloud
+wc.generate(texte)
+
+# store to file
+wc.to_file("WC2_WEED.png")
 
 # show
 plt.imshow(wc, interpolation='bilinear')
